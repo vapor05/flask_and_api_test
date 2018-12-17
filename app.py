78453c5002089d39ceb0879bfdac6648a251a563
NBOCCHINI <nicholasbocchini@gmail.com>
2018-12-16 22:56:50 -0600

    Impemented the first function.  - returns a json dict with all stores in the stores python dict

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -30,7 +30,7 @@ def get_store(name):
 # GET /store
 @app.route('/store')
 def get_stores():
-	pass
+	return jsonify({'stores': stores})
 	
 # POST /store/<string:name>/item {name:, price:}
 @app.route('/store/<string:name>/item', methods=['POST'])
