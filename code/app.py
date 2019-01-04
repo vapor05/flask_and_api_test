6f62bd13ff86948ce6175a5f92cceaf5a01b44d1
NBOCCHINI <nicholasbocchini@gmail.com>
2019-01-03 18:45:35 -0600

    - Added a new Items resource that returns all items stored in the items list - Updated the Item resource to allow post requests to set the price of created items - Added the debug option to the app.run function

--- code/app.py
+++ code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -14,10 +14,16 @@ class Item(Resource):
 		return {'item': None}, 404
 				
 	def post(self, name):
-		item = {'name': name, 'price': 12.0}
+		data = request.get_json()
+		item = {'name': name, 'price': data['price']}
 		items.append(item)
 		return item, 201
 		
+class Items(Resource):
+	def get(self):
+		return {'items': items}
+		
 api.add_resource(Item, '/item/<string:name>') #http://127.0.0.1:5000/item/chair
+api.add_resource(Items, '/items')
 
-app.run(port=5000)
+app.run(port=5000, debug=True)
