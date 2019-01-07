61733cb533d7c6e3e15b2c7f767fdb1a22e038cc
NBOCCHINI <nicholasbocchini@gmail.com>
2019-01-06 19:31:35 -0600

    Implemented the post function

--- code/app.py
+++ code/app.py
@@ -32,6 +32,17 @@ class Item(Resource):
 		items = list(filter(lambda x: x['name'] != name, items))
 		return {'message': 'Item delete'}
 		
+	def put(self, name):
+		data = request.get_json()
+		item = next(filter(lambda x: x['name'] == name, items), None)
+		if item is None:
+			item = {'name': name, 'price': data['price']}
+			items.append(item)
+		else:
+			item.update(data)
+		return item	
+		
+				
 class Items(Resource):
 	def get(self):
 		return {'items': items}
