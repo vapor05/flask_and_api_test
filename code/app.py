bd5a86c4e9e432eeffb369a10dfadff7a2e28494
NBOCCHINI <nicholasbocchini@gmail.com>
2019-01-07 21:07:16 -0600

    Added argument parsering in the json requests for the post and put functions 	- will only accept a 'price' key and data now from the requests

--- code/app.py
+++ code/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -22,7 +22,13 @@ class Item(Resource):
 		if next(filter(lambda x: x['name'] == name, items), None):
 			return {'message': "An item with name '{}' already exists.".format(name)}, 400
 			
-		data = request.get_json()
+		parser = reqparse.RequestParser()
+		parser.add_argument('price',
+			type=float,
+			required=True,
+			help="This field cannot be left blank"
+		)
+		data = parser.parse_args()
 		item = {'name': name, 'price': data['price']}
 		items.append(item)
 		return item, 201
@@ -33,7 +39,13 @@ class Item(Resource):
 		return {'message': 'Item delete'}
 		
 	def put(self, name):
-		data = request.get_json()
+		parser = reqparse.RequestParser()
+		parser.add_argument('price',
+			type=float,
+			required=True,
+			help="This field cannot be left blank"
+		)
+		data = parser.parse_args()
 		item = next(filter(lambda x: x['name'] == name, items), None)
 		if item is None:
 			item = {'name': name, 'price': data['price']}
