4444469a735bf12811d4160db87b6c22ab85c632
NBOCCHINI <nicholasbocchini@gmail.com>
2019-01-08 23:00:31 -0600

    Begining work on creating logic to allow new users to write their usernames and passwords to a stored database

--- code/app.py
+++ code/app.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
+from user import UserRegister
 
 app = Flask(__name__)
 app.secret_key = 'keyValueForSecurity'
@@ -24,21 +25,21 @@ class Item(Resource):
 	def get(self, name):
 		item = next(filter(lambda x: x['name'] == name, items), None)
 		return {'item': item}, 200 if item else 404
-				
+
 	def post(self, name):
 		if next(filter(lambda x: x['name'] == name, items), None):
 			return {'message': "An item with name '{}' already exists.".format(name)}, 400
-			
+
 		data = Item.parser.parse_args()
 		item = {'name': name, 'price': data['price']}
 		items.append(item)
 		return item, 201
-		
+
 	def delete(self, name):
 		global items
 		items = list(filter(lambda x: x['name'] != name, items))
 		return {'message': 'Item delete'}
-		
+
 	def put(self, name):
 		data = Item.parser.parse_args()
 		item = next(filter(lambda x: x['name'] == name, items), None)
@@ -47,14 +48,15 @@ class Item(Resource):
 			items.append(item)
 		else:
 			item.update(data)
-		return item	
-		
-				
+		return item
+
+
 class Items(Resource):
 	def get(self):
 		return {'items': items}
-		
+
 api.add_resource(Item, '/item/<string:name>') #http://127.0.0.1:5000/item/chair
 api.add_resource(Items, '/items')
+api.add_resource(UserRegister, '/register')
 
 app.run(port=5000, debug=True)
