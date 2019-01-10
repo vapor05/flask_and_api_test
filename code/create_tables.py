01fcefaed22e78c73d3f5305595e5f6b106c02c7
NBOCCHINI <nicholasbocchini@gmail.com>
2019-01-09 22:42:13 -0600

    Fixed the new feature. Users can now register a username and password and the app will store them in a sqlite database successfully

--- code/create_tables.py
+++ code/create_tables.py
@@ -3,8 +3,11 @@ import sqlite3
 connection = sqlite3.connect('data.db')
 cursor = connection.cursor()
 
-create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGAR PRIMARY KEY, username text, password text)"
+create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY ASC NOT NULL, username text, password text)"
 cursor.execute(create_table)
+
+insert_query = "INSERT INTO users VALUES (1, 'test', 'testpassword')"
+cursor.execute(insert_query)
 
 connection.commit()
 connection.close()
